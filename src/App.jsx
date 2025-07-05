48769bba8cd26d2a7b5204dd62d8a289c83bdde6
Vijayanandr2000 <vijayanandr2000@gmail.com>
2025-07-05 18:21:13 +0530

    fix(routing): routing fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
-import { useState } from 'react'
-import { HashRouter, Routes , Route } from 'react-router-dom'
-import './App.css'
-import Navbar from './pages/Navbar'
-import Home from './pages/Home'
-import WatchList from './pages/WatchList'
-import { WatchListProvider } from './context/WatchListContext'
+import { useState } from "react";
+import {
+  HashRouter,
+  Routes,
+  Route,
+  BrowserRouter,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
+import Navbar from "./pages/Navbar";
+import Home from "./pages/Home";
+import WatchList from "./pages/WatchList";
+import { WatchListProvider } from "./context/WatchListContext";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <WatchListProvider>
-      <HashRouter>
-      <Navbar/>
-      <Routes>
-        <Route path='/home' element={<Home />}/>
-        <Route path='/watchlist' element={<WatchList/>}/>
-      </Routes>
-    </HashRouter>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/watchlist" element={<WatchList />} />
+
+          <Route path="*" element={<Navigate to="/home" />} />
+        </Routes>
+      </BrowserRouter>
     </WatchListProvider>
-    
-  )
+  );
 }
 
-export default App
+export default App;
